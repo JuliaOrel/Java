0a3072eabd5acbbaedf9cd7633d9f674cc2bc1c3
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-08-16 20:51:55 +0300

    first

---
+++ hello_java/hello_java/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+/*
+F:\Java_jdk\jdk-17\bin\java.exe
+"-javaagent:F:\JetBrains\IntelliJ IDEA 2023.2\lib\idea_rt.jar=59039:F:\JetBrains\IntelliJ IDEA 2023.2\bin"
+-Dfile.encoding=UTF-8
+-classpath C:\Users\User\IdeaProjects\Java\hello_java\hello_java\target\classes org.example.Main
+
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
