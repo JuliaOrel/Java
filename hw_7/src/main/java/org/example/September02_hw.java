315b1ab27eef18266fea265ed0f64aef63d7f7d1
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-09-02 15:46:42 +0300

    db countries

--- hw_7/src/main/java/org/example/September02_hw.java
+++ hw_7/src/main/java/org/example/September02_hw.java
@@ -5,12 +5,18 @@ import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class September02_hw implements Runnable{
     private Connection connection;
     @Override
     public void run() {
-
+        ConnectToDb();
+        try {
+            displayMenu();
+        } catch (SQLException e) {
+            System.out.println("Something wrong"+e.getMessage());
+        }
     }
     private void ConnectToDb(){
         Dotenv dotenv = Dotenv.load();
@@ -39,4 +45,54 @@ public class September02_hw implements Runnable{
             System.out.println("Not connected to the database.");
         }
     }
+    private void displayMenu() throws SQLException{
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Make your choice:");
+            System.out.println("1. Connect to DB");
+            System.out.println("2. Disconnect from DB");
+            System.out.println("3. Display all countries");
+            System.out.println("4. Display all cities of country");
+            System.out.println("5. Display all manufacturers and amount of cars");
+            System.out.println("6. Functions");
+            System.out.println("7. Filters");
+            System.out.println("8. Settings");
+            System.out.println("9. Exit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    ConnectToDb();
+                    break;
+                case 2:
+                    disconnectFromDb();
+                    break;
+                case 3:
+                    //displayAllCars();
+                    break;
+                case 4:
+                    //displayManufacturers();
+                    break;
+                case 5:
+                    //displayManufacturersAndCounts();
+                    break;
+                case 6:
+                    //displayMenuFunct();
+                    break;
+                case 7:
+                    //displayMenuFilters();
+                    break;
+                case 8:
+                    //displayMenuSettings();
+                    break;
+                case 9:
+                    return; // Выход из метода
+                default:
+                    System.out.println("Некорректный выбор. Пожалуйста, выберите снова.");
+            }
+        }
+    }
+
 }
