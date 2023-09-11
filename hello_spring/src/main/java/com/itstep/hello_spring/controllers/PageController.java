f974b737808c7d9919c54955091d385394a7b9a4
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-09-11 19:55:40 +0300

    Models

--- hello_spring/src/main/java/com/itstep/hello_spring/controllers/PageController.java
+++ hello_spring/src/main/java/com/itstep/hello_spring/controllers/PageController.java
@@ -1,12 +1,29 @@
 package com.itstep.hello_spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PageController {
     @GetMapping("/")
     public String index(){
         return ("pages/index");
+    }
+
+    @GetMapping("/thymeleaf")
+    public String buildServer(Model model){
+        List<String> lstData=new ArrayList<>();
+        lstData.add("First element");
+        lstData.add("Second element");
+        lstData.add("Third element");
+
+        model.addAttribute("lstData", lstData);
+
+        model.addAttribute("title", "Hello Models");
+        return ("example/thymeleaf");
     }
 }
