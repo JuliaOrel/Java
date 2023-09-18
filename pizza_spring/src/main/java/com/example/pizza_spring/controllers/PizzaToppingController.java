150f1c5a1756401a068b2a9339f7d01a88868ad1
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-09-19 00:46:44 +0300

    Spring topping controller

--- pizza_spring/src/main/java/com/example/pizza_spring/controllers/PizzaToppingController.java
+++ pizza_spring/src/main/java/com/example/pizza_spring/controllers/PizzaToppingController.java
@@ -1,4 +1,68 @@
 package com.example.pizza_spring.controllers;
 
+import com.example.pizza_spring.models.Pizza;
+import com.example.pizza_spring.models.PizzaOrder;
+import com.example.pizza_spring.models.Topping;
+import com.example.pizza_spring.repositories.PizzaRepository;
+import com.example.pizza_spring.repositories.ToppingRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/topping")
 public class PizzaToppingController {
+
+    private final PizzaRepository pizzaRepository;
+    private final ToppingRepository toppingRepository;
+
+    public PizzaToppingController(PizzaRepository pizzaRepository, ToppingRepository toppingRepository){
+        this.pizzaRepository=pizzaRepository;
+        this.toppingRepository=toppingRepository;
+    }
+
+    //Показывает все топпинги  конкретной пиццы
+    @GetMapping("/{pizzaId}/toppings")
+    public ResponseEntity<List<Topping>> getToppingsForPizza(@PathVariable UUID pizzaId) {
+        Optional<Pizza> pizza = pizzaRepository.findById(pizzaId);
+        if (pizza.isPresent()) {
+            return new ResponseEntity<>(pizza.get().getToppings(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    //Показывает все топпинги
+    @GetMapping
+    public List<Topping> getAllToppings() {
+        return toppingRepository.findAll();
+    }
+
+    //Add new topping
+    @PostMapping("/toppings")
+    public ResponseEntity<Topping> createTopping(@RequestBody Topping topping) {
+        Topping savedOrder = toppingRepository.save(topping);
+        return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
+
+    //add new topping to pizza"
+    @PostMapping("/toppings/{pizzaId}/addTopping/{toppingId}")
+    public ResponseEntity<Pizza> addToppingToPizza(@PathVariable UUID toppingId, @PathVariable UUID pizzaId) {
+        Optional<Topping> optionalTopping = toppingRepository.findById(toppingId);
+        Optional<Pizza> optionalPizza = pizzaRepository.findById(pizzaId);
+
+        if (optionalTopping.isPresent() && optionalPizza.isPresent()) {
+            Topping topping = optionalTopping.get();
+            Pizza pizza = optionalPizza.get();
+
+            pizza.getToppings().add(topping);
+            Pizza updatedPizza = pizzaRepository.save(pizza);
+            return new ResponseEntity<>(updatedPizza, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
