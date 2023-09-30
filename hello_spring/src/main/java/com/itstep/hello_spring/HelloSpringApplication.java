60205a0a6f0dc43af4faa4123aafdd778746317d
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-09-30 17:23:34 +0300

    email jobs

--- hello_spring/src/main/java/com/itstep/hello_spring/HelloSpringApplication.java
+++ hello_spring/src/main/java/com/itstep/hello_spring/HelloSpringApplication.java
@@ -2,8 +2,10 @@ package com.itstep.hello_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class HelloSpringApplication {
 
     public static void main(String[] args) {
