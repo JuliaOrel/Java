3b3c9b0477d60beff826da71a22d9bee69045426
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-09-30 20:21:55 +0300

    email jobs

--- hello_spring/src/main/java/com/itstep/hello_spring/controllers/forServices/ApiAdminPersonController.java
+++ hello_spring/src/main/java/com/itstep/hello_spring/controllers/forServices/ApiAdminPersonController.java
@@ -3,6 +3,7 @@ package com.itstep.hello_spring.controllers.forServices;
 import com.itstep.hello_spring.models.relationship.one_many.SomePerson;
 import com.itstep.hello_spring.services.SomePersonService;
 import com.itstep.hello_spring.services.admin.AdminSomePersonService;
+import com.itstep.hello_spring.services.email.EmailService;
 import com.itstep.hello_spring.viewModels.admin.SomePersonAdminResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiAdminPersonController {
     final SomePersonService personService;
     final AdminSomePersonService adminSomePersonService;
+    final
+    EmailService emailService;
     public ApiAdminPersonController(
             SomePersonService personService,
-            AdminSomePersonService adminSomePersonService
+            AdminSomePersonService adminSomePersonService, EmailService emailService
     ) {
         this.personService = personService;
         this.adminSomePersonService = adminSomePersonService;
+        this.emailService=emailService;
     }
 
     @GetMapping("/")
     public Iterable<SomePerson>getAllPersons(){
+        emailService.sendEmail("juliaorel1507@gmail.com", "Test", "Body");
         return personService.findAll();
     }
     @GetMapping("/ad")
--- hello_spring/src/main/java/com/itstep/hello_spring/services/email/EmailService.java
+++ hello_spring/src/main/java/com/itstep/hello_spring/services/email/EmailService.java
@@ -1,6 +1,6 @@
 package com.itstep.hello_spring.services.email;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
