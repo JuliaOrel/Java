7a6c0b66a225c9d59d780a194cf78d1cc94d6292
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-10-03 22:47:32 +0300

    Web socket

--- hello_spring/src/main/java/com/itstep/hello_spring/HelloSpringApplication.java
+++ hello_spring/src/main/java/com/itstep/hello_spring/HelloSpringApplication.java
@@ -3,9 +3,11 @@ package com.itstep.hello_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
 @EnableAsync
+@EnableWebSocket
 public class HelloSpringApplication {
 
     public static void main(String[] args) {
