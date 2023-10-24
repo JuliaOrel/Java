b014d4aae1dac4c0533af65d2f56169bec88509b
Julia <juliaorel1507@gmail.com>
2023-10-24 22:05:31 +0300

    Poem file fix azure

--- src/main/java/com/example/poemapp/controllers/LyricFromImageController.java
+++ src/main/java/com/example/poemapp/controllers/LyricFromImageController.java
@@ -1,7 +1,6 @@
 package com.example.poemapp.controllers;
 
 import com.example.poemapp.services.AzureVisionService;
-import com.example.poemapp.services.ChatGPTService;
 import com.example.poemapp.services.LocalFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,19 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @RestController
 public class LyricFromImageController {
     @Autowired
     private LocalFileService localFileService;
-//    @Autowired
-//    private AzureVisionService azureVisionService;
-//
-//    @Autowired
-//    private ChatGPTService chatGPTService;
+    @Autowired
+    private AzureVisionService azureVisionService;
+
     @PostMapping("/upload")
     public String upload(
             @RequestParam("file") MultipartFile file
@@ -32,8 +26,8 @@ public class LyricFromImageController {
         }
 
         String filePath = localFileService.uploadFile("ai", file);
-//        String question = azureVisionService.analyzeImageFromFile(filePath);
-//        String lyric = chatGPTService.SendQuestion(question + "\n на русском языке");
-        return "some string";
+        String question = azureVisionService.analyzeImageFromFile(filePath);
+        //String lyric = chatGPTService.SendQuestion(question + "\n на русском языке");
+        return question;
     }
 }
