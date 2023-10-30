cfebaa7c4e3dc1ea429dbbcee56a415b639cb7df
Julia <juliaorel1507@gmail.com>
2023-10-30 22:22:26 +0200

    calculator

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_frame_layout);
+        setContentView(R.layout.calculator_layout);
 //        MyRegisterFormValidator validator=new MyRegisterFormValidator(this);
 //        ((EditText)findViewById(R.id.inpEmail)).addTextChangedListener(validator);
 //        ((EditText)findViewById(R.id.inpPassword)).addTextChangedListener(validator);
