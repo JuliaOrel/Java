dfd48da546dea405847b8ea0bb1614c5e2281e28
Julia <juliaorel1507@gmail.com>
2023-11-04 15:03:15 +0200

    web sockets

--- src/main/java/com/example/poemapp/PoemAppApplication.java
+++ src/main/java/com/example/poemapp/PoemAppApplication.java
@@ -3,9 +3,11 @@ package com.example.poemapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
 @EnableAsync
+@EnableWebSocket
 public class PoemAppApplication {
 
 	public static void main(String[] args) {
